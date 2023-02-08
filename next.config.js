69e22a071b5fcae1abfcc93b3a4fb0bb99f202ea
Andrey Potyomkin <and_tem@icloud.com>
2023-02-08 09:48:30 +0300

    chore: Настроен next

--- next.config.js
+++ next.config.js
@@ -1,27 +1,13 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const withTM = require('next-transpile-modules')(['@astral/ui']);
+const withTM = require('next-transpile-modules')([
+  '@astral/ui/fonts',
+  '@astral/ui/illustrations',
+]);
 const { withSentryConfig } = require('@sentry/nextjs');
 
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  experimental: {
-    esmExternals: true,
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(woff|woff2)$/i,
-      issuer: { and: [/\.(js|ts)x?$/] },
-      type: 'asset/resource',
-    });
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = withSentryConfig(
   {
@@ -33,5 +19,5 @@ module.exports = withSentryConfig(
   },
   {
     silent: true,
-  }
+  },
 );
--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN =
   process.env.SENTRY_DSN ||
   process.env.NEXT_PUBLIC_SENTRY_DSN |
-  'https://f6bd384734374955a4bc0272d081956b@sentry.infra.yandex.astral-dev.ru/152';
+    'https://f6bd384734374955a4bc0272d081956b@sentry.infra.yandex.astral-dev.ru/152';
 
 Sentry.init({
   dsn: SENTRY_DSN,
