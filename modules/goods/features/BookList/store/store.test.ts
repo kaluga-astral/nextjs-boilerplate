c8e085041af47f5d7427c249d9b73537a2e6fea4
Andrey Potyomkin <and_tem@icloud.com>
2023-11-08 10:30:40 +0300

    test: Добавлены примеры стабов сорсов

--- modules/goods/features/BookList/store/store.test.ts
+++ modules/goods/features/BookList/store/store.test.ts
@@ -26,4 +26,26 @@ describe('GoodsListStore', () => {
       price: '1 000 руб.',
     });
   });
+
+  // with sources stub
+  // it('Форматирует list для отображения', async () => {
+  //   const fakeBookList = bookRepositoryFaker.makeBookList(2, { price: 1000 });
+  //   const fakeBookListItem = fakeBookList.data[0];
+  //
+  //   const bookNetworkSourcesStub = mock<BookNetworkSources>({
+  //     getBookList: async () => makeFakeSourceRes(fakeBookList),
+  //   });
+  //   const sut = new GoodsListStore(
+  //     new BookRepository(bookNetworkSourcesStub, cacheService),
+  //   );
+  //
+  //   // ждем автоматической загрузки данных
+  //   await when(() => Boolean(sut.list?.length));
+  //
+  //   expect(sut.list[0]).toContain({
+  //     id: fakeBookListItem.id,
+  //     name: fakeBookListItem.name,
+  //     price: '1 000 руб.',
+  //   });
+  // });
 });
--- modules/payment/domain/CardPaymentStore/CardPaymentStore.test.ts
+++ modules/payment/domain/CardPaymentStore/CardPaymentStore.test.ts
@@ -28,6 +28,32 @@ describe('CardPaymentStore', () => {
     ]);
   });
 
+  // it('Отправляет на оплату все товары, добавленные в корзину. With sources stub', () => {
+  //   const fakeGoodsList = cartRepositoryFaker.makeGoodsList(2);
+  //
+  //   const mutationMock =
+  //     mock<ReturnType<PaymentRepository['createPaymentByCardMutation']>>();
+  //
+  //   const paymentRepositoryMock = mock<PaymentRepository>({
+  //     createPaymentByCardMutation: () => mutationMock,
+  //   });
+  //   const notifyService = mock<typeof notify>();
+  //   const sut = new CardPaymentStore(
+  //     new CartStore(
+  //       new CartRepository(fakeCartNetworkSources, cacheService),
+  //       notifyService,
+  //     ),
+  //     new PaymentRepository(new CartRepository()),
+  //   );
+  //
+  //   sut.pay();
+  //
+  //   expect(mutationMock.sync.mock.lastCall?.[0]?.params).toEqual([
+  //     fakeGoodsList[0].id,
+  //     fakeGoodsList[1].id,
+  //   ]);
+  // });
+
   // it('Форматирует ошибки оплаты для отображения', async () => {
   //   const cartStoreStub = mock<CartStore>({ goods: [] });
   //   const paymentRepositoryMock = mock<PaymentRepository>({
